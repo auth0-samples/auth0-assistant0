AdamWozniewski/auth0-assistant0-py-llamaindex

AIDX-237 Switch call method

ts-langchain/src/components/auth0-ai/TokenVault/popup.tsx
@@ -7,9 +7,9 @@ import { PromptUserContainer } from '../util/prompt-user-container';
 import { TokenVaultAuthProps } from './TokenVaultAuthProps';
 
 export function TokenVaultConsentPopup({
-  interrupt: { connection, requiredScopes, resume },
+  interrupt: { connection, requiredScopes, resume, authorizationParams },
   connectWidget: { icon, title, description, action, containerClassName },
-  auth: { authorizePath = '/auth/login', returnTo = '/close' } = {},
+  auth: { connectPath = '/auth/connect', returnTo = '/close' } = {},
   onFinish,
 }: TokenVaultAuthProps) {
   const [isLoading, setIsLoading] = useState(false);
@@ -43,14 +43,15 @@ export function TokenVaultConsentPopup({
   //Open the login popup
   const startLoginPopup = useCallback(async () => {
     const search = new URLSearchParams({
-      returnTo,
       connection,
-      access_type: 'offline',
-      prompt: 'consent',
-      connection_scope: requiredScopes.join(),
+      returnTo,
+      scope: requiredScopes.join(" "),
+      // Add all extra authorization parameters to the search params, they will be collected and submitted via the
+      // authorization_params parameter of the connect account flow.
+      ...authorizationParams,
     });
 
-    const url = new URL(authorizePath, window.location.origin);
+    const url = new URL(connectPath, window.location.origin);
     url.search = search.toString();
 
     const windowFeatures = 'width=800,height=650,status=no,toolbar=no,menubar=no';
@@ -62,7 +63,7 @@ export function TokenVaultConsentPopup({
       setLoginPopup(popup);
       setIsLoading(true);
     }
-  }, [connection, requiredScopes, returnTo, authorizePath]);
+  }, [connection, requiredScopes, returnTo, connectPath, authorizationParams]);
 
   if (isLoading) {
     return <WaitingMessage />;

ts-langchain/src/components/auth0-ai/TokenVault/redirect.tsx
@@ -4,9 +4,9 @@ import { PromptUserContainer } from '../util/prompt-user-container';
 import { TokenVaultAuthProps } from './TokenVaultAuthProps';
 
 export function TokenVaultConsentRedirect({
-  interrupt: { requiredScopes, connection },
+  interrupt: { requiredScopes, connection, authorizationParams },
   connectWidget: { icon, title, description, action, containerClassName },
-  auth: { authorizePath = '/auth/login', returnTo = window.location.pathname } = {},
+  auth: { connectPath = '/auth/connect', returnTo = window.location.pathname } = {},
 }: TokenVaultAuthProps) {
   return (
     <PromptUserContainer
@@ -17,14 +17,16 @@ export function TokenVaultConsentRedirect({
       action={{
         label: action?.label ?? 'Connect',
         onClick: () => {
-          const search = new URLSearchParams({
-            returnTo,
-            connection,
-            access_type: 'offline',
-            connection_scope: requiredScopes.join(),
-          });
+            const search = new URLSearchParams({
+                connection,
+                returnTo,
+                scope: requiredScopes.join(" "),
+                // Add all extra authorization parameters to the search params, they will be collected and submitted via the
+                // authorization_params parameter of the connect account flow.
+                ...authorizationParams,
+            });
 
-          const url = new URL(authorizePath, window.location.origin);
+          const url = new URL(connectPath, window.location.origin);
           url.search = search.toString();
 
           // Redirect to the authorization page

ts-vercel-ai/src/components/auth0-ai/TokenVault/popup.tsx
@@ -7,9 +7,9 @@ import { PromptUserContainer } from "../util/prompt-user-container";
 import { TokenVaultAuthProps } from "./TokenVaultAuthProps";
 
 export function TokenVaultConsentPopup({
-  interrupt: { connection, requiredScopes, resume },
+  interrupt: { connection, requiredScopes, resume, authorizationParams },
   connectWidget: { icon, title, description, action, containerClassName },
-  auth: { authorizePath = "/auth/login", returnTo = "/close" } = {},
+  auth: { connectPath = "/auth/login", returnTo = "/close" } = {},
   onFinish,
 }: TokenVaultAuthProps) {
   const [isLoading, setIsLoading] = useState(false);
@@ -50,7 +50,7 @@ export function TokenVaultConsentPopup({
       connection_scope: requiredScopes.join(),
     });
 
-    const url = new URL(authorizePath, window.location.origin);
+    const url = new URL(connectPath, window.location.origin);
     url.search = search.toString();
 
     const windowFeatures =
@@ -63,7 +63,7 @@ export function TokenVaultConsentPopup({
       setLoginPopup(popup);
       setIsLoading(true);
     }
-  }, [connection, requiredScopes]);
+  }, [connection, requiredScopes, returnTo, authorizationParams, connectPath]);
 
   if (isLoading) {
     return <WaitingMessage />;

auth0-samples/auth0-assistant0

ts-langchain/src/components/auth0-ai/TokenVault/popup.tsx
@@ -7,9 +7,9 @@ import { PromptUserContainer } from '../util/prompt-user-container';
 import { TokenVaultAuthProps } from './TokenVaultAuthProps';
 
 export function TokenVaultConsentPopup({
-  interrupt: { connection, requiredScopes, resume },
+  interrupt: { connection, requiredScopes, resume, authorizationParams },
   connectWidget: { icon, title, description, action, containerClassName },
-  auth: { authorizePath = '/auth/login', returnTo = '/close' } = {},
+  auth: { connectPath = '/auth/login', returnTo = '/close' } = {},
   onFinish,
 }: TokenVaultAuthProps) {
   const [isLoading, setIsLoading] = useState(false);
@@ -43,14 +43,15 @@ export function TokenVaultConsentPopup({
   //Open the login popup
   const startLoginPopup = useCallback(async () => {
     const search = new URLSearchParams({
-      returnTo,
       connection,
-      access_type: 'offline',
-      prompt: 'consent',
-      connection_scope: requiredScopes.join(),
+      returnTo,
+      scope: requiredScopes.join(" "),
+      // Add all extra authorization parameters to the search params, they will be collected and submitted via the
+      // authorization_params parameter of the connect account flow.
+      ...authorizationParams,
     });
 
-    const url = new URL(authorizePath, window.location.origin);
+    const url = new URL(connectPath, window.location.origin);
     url.search = search.toString();
 
     const windowFeatures = 'width=800,height=650,status=no,toolbar=no,menubar=no';
@@ -62,7 +63,7 @@ export function TokenVaultConsentPopup({
       setLoginPopup(popup);
       setIsLoading(true);
     }
-  }, [connection, requiredScopes, returnTo, authorizePath]);
+  }, [connection, requiredScopes, returnTo, connectPath, authorizationParams]);
 
   if (isLoading) {
     return <WaitingMessage />;

ts-langchain/src/components/auth0-ai/TokenVault/redirect.tsx
@@ -4,9 +4,9 @@ import { PromptUserContainer } from '../util/prompt-user-container';
 import { TokenVaultAuthProps } from './TokenVaultAuthProps';
 
 export function TokenVaultConsentRedirect({
-  interrupt: { requiredScopes, connection },
+  interrupt: { requiredScopes, connection, authorizationParams },
   connectWidget: { icon, title, description, action, containerClassName },
-  auth: { authorizePath = '/auth/login', returnTo = window.location.pathname } = {},
+  auth: { connectPath = '/auth/login', returnTo = window.location.pathname } = {},
 }: TokenVaultAuthProps) {
   return (
     <PromptUserContainer
@@ -17,14 +17,16 @@ export function TokenVaultConsentRedirect({
       action={{
         label: action?.label ?? 'Connect',
         onClick: () => {
-          const search = new URLSearchParams({
-            returnTo,
-            connection,
-            access_type: 'offline',
-            connection_scope: requiredScopes.join(),
-          });
+            const search = new URLSearchParams({
+                connection,
+                returnTo,
+                scope: requiredScopes.join(" "),
+                // Add all extra authorization parameters to the search params, they will be collected and submitted via the
+                // authorization_params parameter of the connect account flow.
+                ...authorizationParams,
+            });
 
-          const url = new URL(authorizePath, window.location.origin);
+          const url = new URL(connectPath, window.location.origin);
           url.search = search.toString();
 
           // Redirect to the authorization page

ts-vercel-ai/src/components/auth0-ai/TokenVault/popup.tsx
@@ -7,9 +7,9 @@ import { PromptUserContainer } from "../util/prompt-user-container";
 import { TokenVaultAuthProps } from "./TokenVaultAuthProps";
 
 export function TokenVaultConsentPopup({
-  interrupt: { connection, requiredScopes, resume },
+  interrupt: { connection, requiredScopes, resume, authorizationParams },
   connectWidget: { icon, title, description, action, containerClassName },
-  auth: { authorizePath = "/auth/login", returnTo = "/close" } = {},
+  auth: { connectPath = "/auth/connect", returnTo = "/close" } = {},
   onFinish,
 }: TokenVaultAuthProps) {
   const [isLoading, setIsLoading] = useState(false);
@@ -50,7 +50,7 @@ export function TokenVaultConsentPopup({
       connection_scope: requiredScopes.join(),
     });
 
-    const url = new URL(authorizePath, window.location.origin);
+    const url = new URL(connectPath, window.location.origin);
     url.search = search.toString();
 
     const windowFeatures =
@@ -63,7 +63,7 @@ export function TokenVaultConsentPopup({
       setLoginPopup(popup);
       setIsLoading(true);
     }
-  }, [connection, requiredScopes]);
+  }, [connection, requiredScopes, returnTo, authorizationParams, connectPath]);
 
   if (isLoading) {
     return <WaitingMessage />;