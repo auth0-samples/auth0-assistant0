b9c31b8544c07c5fdb6ff8bf8c3774a3b9d4ccfe
Patrick Malouin <patrick.malouin@okta.com>
2025-11-19 15:14:22 -0500

    feat(py-langchain): add support for connected accounts

--- py-langchain/frontend/src/lib/api-client.ts
+++ py-langchain/frontend/src/lib/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_HOST,
+  baseURL: window.location.origin,
   withCredentials: true,
 });
--- py-langchain/frontend/vite.config.ts
+++ py-langchain/frontend/vite.config.ts
@@ -1,14 +1,24 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.API_HOST,
+        },
+      },
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
